747de3e2a4eb699ce3a716fcf50e550b7a5c1a0d
ZSANG <sang0851755904@hotmial.com>
2020-09-09 13:39:57 +0700

    Histogram Function

--- histograam.py
+++ histograam.py
@@ -2,20 +2,31 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+bit_depth = 8
+max_intensity = pow(2,bit_depth)
+
 img = cv2.imread('8_bit.png',0)
-print(img.shape)
+# print(img.shape)
 cv2.imshow("image",img)
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
-plt.hist(img.ravel(),256,[0,256]); plt.show()
+# plt.hist(img.ravel(),256,[0,256]); plt.show()
+
 list_img = []
+intensity = [0 for i in range(max_intensity)]
+
 for row,index in enumerate(img):
     # print(row)
     list_img = np.concatenate((list_img,index))
-print(len(list_img))
-color = ('b')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([img],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
+
+for i in list_img:
+    intensity[int(i)] +=1
+
+histr = cv2.calcHist([img],[0],None,[max_intensity],[0,max_intensity])
+
+plt.figure("Histogram")
+plt.subplot(2,1,1)
+plt.plot(intensity)
+plt.subplot(2,1,2)
+plt.plot(histr)
 plt.show()
