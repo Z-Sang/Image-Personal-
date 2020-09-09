08984ea963e306962db776adaf4a3045384f2e1a
ZSANG <sang0851755904@hotmial.com>
2020-09-09 13:57:15 +0700

    Add title

--- histograam.py
+++ histograam.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-bit_depth = 8
-max_intensity = pow(2,bit_depth)
+min_intensity = 0
+max_intensity = 256
 
 img = cv2.imread('8_bit.png',0)
 # print(img.shape)
-cv2.imshow("image",img)
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
+# cv2.imshow("image",img)
+# cv2.waitKey(0) 
+# cv2.destroyAllWindows()
 # plt.hist(img.ravel(),256,[0,256]); plt.show()
 
 list_img = []
@@ -22,11 +22,13 @@ for row,index in enumerate(img):
 for i in list_img:
     intensity[int(i)] +=1
 
-histr = cv2.calcHist([img],[0],None,[max_intensity],[0,max_intensity])
+histr = cv2.calcHist([img],[0],None,[max_intensity],[min_intensity,max_intensity])
 
 plt.figure("Histogram")
 plt.subplot(2,1,1)
+plt.title("No Tool")
 plt.plot(intensity)
 plt.subplot(2,1,2)
+plt.title("From CV")
 plt.plot(histr)
 plt.show()
