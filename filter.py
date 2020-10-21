ce76d19ec1f566d16bda22ded323d1447050a558
ZSANG <sang0851755904@hotmial.com>
2020-10-21 12:46:29 +0700

    unsuccess filter

--- filter.py
+++ filter.py
@@ -13,3 +13,36 @@ def calculateHistogram(img):
     for i in list_img:
         intensity[int(i)] +=1
     return intensity
+
+def filter_image(img,coef):
+    height,width = img.shape
+    filter_img = np.array([[0]*width]*height)
+    for i in range(height):
+        for j in range(width):
+            if i == 0 and j == 0:
+                filter_img[i][j] = 1/9*(img[i][j] * coef[0][0] + img[i][j] * coef[0][1] + img[i][j+1] * coef[0][2] 
+                                        + img[i][j] * coef[1][0] + img[i][j] * coef[1][1] + img[i][j+1] * coef[1][2]
+                                        + img[i+1][j] * coef[2][0] + img[i+1][j] * coef[2][1] + img[i+1][j+1] * coef[2][2])
+            elif i == 0:
+                filter_img[i][j] = 1/9*(img[i][j-1] * coef[0][0] + img[i][j] * coef[0][1] + img[i][j+1] * coef[0][2] 
+                                        + img[i][j-1] * coef[1][0] + img[i][j] * coef[1][1] + img[i][j+1] * coef[1][2]
+                                        + img[i+1][j-1] * coef[2][0] + img[i+1][j] * coef[2][1] + img[i+1][j+1] * coef[2][2])
+            elif j == 0: 
+                filter_img[i][j] = 1/9*(img[i-1][j] * coef[0][0] + img[i-1][j] * coef[0][1] + img[i-1][j+1] * coef[0][2] 
+                                        + img[i][j] * coef[1][0] + img[i][j] * coef[1][1] + img[i][j+1] * coef[1][2]
+                                        + img[i+1][j] * coef[2][0] + img[i+1][j] * coef[2][1] + img[i+1][j+1] * coef[2][2])
+            else:
+                filter_img[i][j] = 1/9*(img[i-1][j-1] * coef[0][0] + img[i-1][j] * coef[0][1] + img[i-1][j+1] * coef[0][2] 
+                                        + img[i][j-1] * coef[1][0] + img[i][j] * coef[1][1] + img[i][j+1] * coef[1][2]
+                                        + img[i+1][j-1] * coef[2][0] + img[i+1][j] * coef[2][1] + img[i+1][j+1] * coef[2][2])
+    
+    return filter_img
+
+img = cv2.imread('talay.jpg',0)
+histogram = calculateHistogram(img)
+img2 = cv2.imread('talay.jpg',0)
+histogram2 = calculateHistogram(img2)
+coef = [[0,0,0],[0,1,0],[0,0,0]]
+img3 =  np.array(filter_image(img2,coef),dtype='uint8_t')
+cv2.imshow('output',img3)
+cv2.waitKey(0)
